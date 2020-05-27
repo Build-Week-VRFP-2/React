49af03547b4c41d9482e659e276b1aac377d4176
unknown <william.z.wilson95@gmail.com>
2020-05-26 20:48:34 -0500

    ayy

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,41 +1,26 @@
 import React, { useState, useEffect } from "react";
-import { Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { Login } from "./components/Login";
 import { Signup } from "./components/Signup";
 import { Nav } from "./components/Nav";
-
 
 import { InvestorSignIn } from "./components/InvestorSignIn";
 import { ApplicantSignIn } from "./components/ApplicantSignIn";
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/signup/investor" component={InvestorSignIn} />
-        <Route path="/signup/applicant" component={ApplicantSignIn} />
-
-
-
-
-
-
-
-        {/* <Route
-          path="/signup"
-          render={(props) => (
-            <div>
-              <Signup />
-              <Test />
-            </div>
-          )}
-        /> */}
-        )} />
-      </Switch>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/signup/investor" component={InvestorSignIn} />
+          <Route path="/signup/applicant" component={ApplicantSignIn} />
+          )} 
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
